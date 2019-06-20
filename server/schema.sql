5ccb1df68a3c5e0514ab7abfcb501def9320173d
cnsarmi <christiannsarmiento@gmail.com>
2019-06-20 12:35:07 -0700

    Schema defines some tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,20 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  user TEXT NOT NULL
+);
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  tweet TEXT NOT NULL,
+  userId INTEGER,
+  postTime DATETIME,
+  FOREIGN KEY (userId) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
